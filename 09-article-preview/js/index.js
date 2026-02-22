e4a762c5d84b00d73445721d4239aad4174b266e
Brandon Catubig <bcatubig@pm.me>
2026-02-21 18:46:14 -0800

    checkpoint

--- 09-article-preview/js/index.js
+++ 09-article-preview/js/index.js
@@ -1,1 +1,10 @@
-console.log("site loaded")
+const shareBtn = document.getElementById("share")
+const mobileShareBtn = document.getElementById("mobileShare")
+
+shareBtn.addEventListener("click", (e) => {
+  console.log("share button clicked!")
+})
+
+mobileShareBtn.addEventListener("click", (e) => {
+  console.log("mobile share button clicked")
+})
